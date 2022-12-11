b61f0c1ee04da2099b71f3a09d122e6d57836724
EvgeniiPliusnin <ea.plyusnin@gmail.com>
2022-12-11 23:24:44 +0300

    added function that clears the terminal

--- Homework/homework_03/guess_number.cpp
+++ Homework/homework_03/guess_number.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <ctime>
+#include <cstdlib>
 
 void print_welcome(void) {
     std::cout << "\n-------------------------------------------------------" << std::endl;
@@ -36,6 +37,18 @@ void safe_table(void) {
 void show_table(void) {
 
 }
+
+void clear_terminal() {
+#if defined _WIN32
+    system("cls");
+    //clrscr(); // including header file : conio.h
+#elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+    system("clear");
+    //std::cout<< u8"\033[2J\033[1;1H"; //Using ANSI Escape Sequences 
+#elif defined (__APPLE__)
+    system("clear");
+#endif
+} 
 
 
 
