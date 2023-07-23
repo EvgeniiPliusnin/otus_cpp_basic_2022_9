488659f3aad136e7a18456087d9e6c029a9b436f
evgenii pliusnin <plyusnin-ea@mail.ru>
2023-07-23 22:10:42 +0300

    init project and dummy lib

---
+++ homeworks/homework_06_lesson_14/homework_06_containers/custom_containers_lib/src/test.cpp
@@ -0,0 +1,11 @@
+//
+// Created by user on 23.07.23.
+//
+
+#include "test.hpp"
+
+#include <iostream>
+
+void hello_from_contaier_lib() {
+    std::cout << "hello_from_container_lib" << std::endl;
+}
---
+++ homeworks/homework_06_lesson_14/homework_06_containers/src/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by user on 23.07.23.
+//
+
+#include <iostream>
+
+#include "test.hpp"
+
+int main(int args, char* argv[]) {
+    std::cout << "hello from main" << std::endl;
+    hello_from_contaier_lib();
+}
