0664804b9ff99ccfbfa1ab5f7ecf41066f614adf
evgenii pliusnin <plyusnin-ea@mail.ru>
2023-08-11 17:38:00 +0300

    dummy

--- homeworks/homework_06_lesson_14/homework_06_containers/src/main.cpp
+++ homeworks/homework_06_lesson_14/homework_06_containers/src/main.cpp
@@ -7,6 +7,12 @@
 #include "test.hpp"
 
 int main(int args, char* argv[]) {
-    std::cout << "hello from main" << std::endl;
-    hello_from_contaier_lib();
+//    std::cout << "hello from main" << std::endl;
+//    hello_from_contaier_lib();
+
+    /*
+     *
+     */
+
+    return 0;
 }
