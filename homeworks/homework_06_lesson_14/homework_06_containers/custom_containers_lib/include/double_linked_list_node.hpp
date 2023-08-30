fd36baf0b8710da352644ed720e79964cc03ee9c
evgenii pliusnin <plyusnin-ea@mail.ru>
2023-08-30 12:40:41 +0300

    skeleton of CustomList completed

---
+++ homeworks/homework_06_lesson_14/homework_06_containers/custom_containers_lib/include/double_linked_list_node.hpp
@@ -0,0 +1,47 @@
+//
+// Created by user on 26.08.23.
+//
+
+#ifndef CUSTOM_CONTAINERS_DOUBLE_LINKED_LIST_NODE_HPP
+#define CUSTOM_CONTAINERS_DOUBLE_LINKED_LIST_NODE_HPP
+
+
+namespace custom_containers {
+
+    template<typename T>
+    class ListNode {
+    public:
+        explicit ListNode(T value) : m_data(value){}
+
+        ListNode(const ListNode<T> &other) {
+            this->m_data = other.m_data;
+            this->m_next_node = other.m_next_node;
+            this->m_prev_node = other.m_prev_node;
+        }
+
+        ListNode(ListNode<T> &&other) noexcept {
+
+        }
+
+        ~ListNode() = default;
+
+        ListNode& operator= (const ListNode<T> &other) {
+            this->m_next_node = other.m_next_node;
+            this->m_prev_node = other.m_prev_node;
+            this->m_data = other.m_data;
+        }
+
+        ListNode& operator= (ListNode<T> &&other)  noexcept {
+//            TODO: learn how to use move assigment operator
+        }
+
+    public:
+        T m_data;
+        ListNode *m_prev_node = nullptr;
+        ListNode *m_next_node = nullptr;
+
+    };
+
+}
+
+#endif //CUSTOM_CONTAINERS_DOUBLE_LINKED_LIST_NODE_HPP
