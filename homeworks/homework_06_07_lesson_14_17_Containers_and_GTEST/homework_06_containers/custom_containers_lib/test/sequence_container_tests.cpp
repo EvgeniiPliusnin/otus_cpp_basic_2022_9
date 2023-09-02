ca52ad42f1456ad47853db0fbb33e433cd4b158b
evgenii pliusnin <plyusnin-ea@mail.ru>
2023-09-02 22:15:00 +0300

    GTest library add

--- homeworks/homework_06_07_lesson_14_17_Containers_and_GTEST/homework_06_containers/custom_containers_lib/test/sequence_container_tests.cpp
+++ homeworks/homework_06_07_lesson_14_17_Containers_and_GTEST/homework_06_containers/custom_containers_lib/test/sequence_container_tests.cpp
@@ -1,3 +1,20 @@
 //
 // Created by user on 30.08.23.
 //
+
+#include <gtest/gtest.h>
+
+#include "custom_sequence_container.hpp"
+
+using namespace custom_containers;
+
+TEST(List, Empty) {
+
+    // Arrange
+    CustomSequenceContainer<int> sequence_container = CustomSequenceContainer{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    // Act (empty for this test)
+
+    // Assert
+//    ASSERT_EQ(sequence_container.size(), static_cast<size_t>(0));
+//    ASSERT_TRUE(sequence_container.empty());
+}
